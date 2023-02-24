ad82f9222b4216a34313451679e3e1b3276c1753
Duy Anh <timdeveloper97@gmail.com>
2023-02-24 17:16:31 +0700

    account page ui

---
+++ src/mobile/xfLibrary/xfLibrary/xfLibrary/Views/ItemView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xfLibrary.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemView : ContentView
+    {
+        public ItemView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create("Text", typeof(string), typeof(ItemView), default(string));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly BindableProperty IconProperty =
+            BindableProperty.Create("Icon", typeof(string), typeof(ItemView), default(string));
+
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly BindableProperty ColorProperty =
+            BindableProperty.Create("Color", typeof(Color), typeof(ItemView), default(Color));
+
+        public Color Color
+        {
+            get { return (Color)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+    }
+}
