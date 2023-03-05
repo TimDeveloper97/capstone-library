f190cfe8fef8234b42623c909da2b46965a37299
zenson22 <asd.skyer3@gmail.com>
2023-03-05 08:13:23 +0700

    - Add imsc things

--- src/server/capstone/src/main/java/com/sb/brothers/capstone/global/GlobalData.java
+++ src/server/capstone/src/main/java/com/sb/brothers/capstone/global/GlobalData.java
@@ -8,9 +8,18 @@ import java.util.List;
 public class GlobalData {
     //tao bien toan cuc
     public static List<Book> cart;
+    public static int cntMess = 0;
+    public static String SUBJECT_MAIL = "[CAPSTONE][REP] Lấy lại mật khẩu";
 
     static {
         cart = new ArrayList<>();
     }
 
+    public static String getSubject(){
+        return SUBJECT_MAIL + " No #" + cntMess++;
+    }
+
+    public static String getContent(String content){
+        return new String("New your password is: " + content +". Please change password after next login.");
+    }
 }
