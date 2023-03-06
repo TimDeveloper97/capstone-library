fce93db193f75537cd296d78ccf9720051e3fa9e
Duy Anh <timdeveloper97@gmail.com>
2023-03-06 16:41:31 +0700

    uypdate choose list category

--- src/mobile/xfLibrary/xfLibrary/xfLibrary/ViewModels/AddPostViewModel.cs
+++ src/mobile/xfLibrary/xfLibrary/xfLibrary/ViewModels/AddPostViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
+using XF.Material.Forms.UI.Dialogs;
 using xfLibrary.Domain;
 using xfLibrary.Pages;
 
@@ -12,10 +13,11 @@ namespace xfLibrary.ViewModels
     class AddPostViewModel : BaseViewModel
     {
         #region Property
-        private ObservableCollection<string> categorys, slides;
+        private ObservableCollection<string> yesNo, slides, category;
 
-        public ObservableCollection<string> Categorys { get => categorys; set => SetProperty(ref categorys, value); }
+        public ObservableCollection<string> YesNo { get => yesNo; set => SetProperty(ref yesNo, value); }
         public ObservableCollection<string> Slides { get => slides; set => SetProperty(ref slides, value); }
+        public ObservableCollection<string> Category { get => category; set => SetProperty(ref category, value); }
 
         #endregion
 
@@ -23,6 +25,13 @@ namespace xfLibrary.ViewModels
 
         public ICommand BackCommand => new Command(async () => await Shell.Current.GoToAsync(".."));
         public ICommand PostCommand => new Command(async () => { });
+        public ICommand CategoryCommand => new Command(async () => {
+            var jobs = Category;
+
+            var result = await MaterialDialog.Instance.SelectChoicesAsync(title: "Chọn thể loại sách",
+                                                                         choices: jobs, dismissiveText: "Hủy", );
+
+        });
         #endregion
 
         public AddPostViewModel()
@@ -33,7 +42,8 @@ namespace xfLibrary.ViewModels
         #region Method
         void Init()
         {
-            Categorys = new ObservableCollection<string> { "Kinh dị", "Tình cảm", "Hành động", "Hài hước", "18+", "Manhwa", "Fantasy" };
+            YesNo = new ObservableCollection<string> { "Có", "Không"};
+            Category = new ObservableCollection<string> { "kinh dị", "Hành động", "Tâm lý", "Tình cảm", "Góc nhìn thứ 1", "Nhập vai", "3D", "Hoạt hình"};
             Slides = new ObservableCollection<string> { "event1.png", "event2.png", "event3.png" };
             IsBusy = true;
         }
