905823815a7e66ded7dfcf552d2cfd891c415868
Tim1997 <timbkhn@gmail.com>
2023-03-09 18:24:10 +0700

    install axios

---
+++ src/web/capstone-client-ui/src/helper/axios.js
@@ -0,0 +1,13 @@
+
+import axios from "axios";
+
+const token = window.localStorage.getItem("token");
+//console.log(token);
+const axiosIntance = axios.create({
+  baseURL: "http://localhost:8090/api/",
+  headers: {
+    Authorization: token ? token : "",
+  },
+});
+
+export default axiosIntance;
--- src/web/capstone-client-ui/src/views/Login/Login.jsx
+++ src/web/capstone-client-ui/src/views/Login/Login.jsx
@@ -1,26 +1,80 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import Input from "../../components/Input/Input";
-import './login.css';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import "./login.css";
+import axiosIntance from "../../helper/axios";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import "react-notifications/lib/notifications.css";
+
+const schema = yup.object({
+  username: yup.string().required("Tên đăng nhập không được để trống"),
+  password: yup.string().required("Mật khẩu không được để trống"),
+});
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    axiosIntance
+      .post("/login", data)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        if(err.response.status === 401){
+          NotificationManager.error("Tên đăng nhập hoặc mật khẩu không chính xác", "Thông báo", 2000);
+        }
+      });
+  };
 
-    const handleSubmit = () => {
-        
-    }
   return (
     <>
-      <Form className="body-content">
+      <Form className="body-content" onSubmit={handleSubmit(onSubmit)}>
+      <NotificationContainer />
         <div className="login-form">
           <h3>Đăng nhập</h3>
-          <Input type="text" placeholder="Nhập số điện thoại" />
-          <Form.Control type="password" placeholder="Nhập mật khẩu của bạn" />
-          <Button variant="primary" onClick={handleSubmit}>
+          <Form.Control
+            type="text"
+            placeholder="Nhập số điện thoại"
+            {...register("username")}
+          />
+          {errors.username && (
+            <span className="error-message" role="alert">
+              {errors.username?.message}
+            </span>
+          )}
+          <Form.Control
+            type="password"
+            placeholder="Nhập mật khẩu của bạn"
+            {...register("password")}
+          />
+          {errors.password && (
+            <span className="error-message" role="alert">
+              {errors.password?.message}
+            </span>
+          )}
+          <Button variant="primary" type="submit">
             Đăng nhập
           </Button>
           <a href="#">Quên mật khẩu?</a>
-          <p>Chưa có tài khoản? <span><Link to={'/register'}>Đăng ký ngay</Link></span></p>
+          <p>
+            Chưa có tài khoản?{" "}
+            <span>
+              <Link to={"/register"}>Đăng ký ngay</Link>
+            </span>
+          </p>
         </div>
       </Form>
     </>
