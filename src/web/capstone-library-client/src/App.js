01c2ad1f03d005d572e171af4d01e00f2daa52ef
Tim1997 <timbkhn@gmail.com>
2023-03-30 16:06:24 +0700

    fix error in header

--- src/web/capstone-library-client/src/App.js
+++ src/web/capstone-library-client/src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Route from './routes';
 
+
 function App() {
+  const pathname = useLocation();
+  useEffect(() => {
+    window.scroll({top: 0, left: 0, behavior: 'smooth'})
+  }, [pathname])
   return <Route />
 }
 
