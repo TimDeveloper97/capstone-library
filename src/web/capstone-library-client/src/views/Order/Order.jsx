a9b315edfcbc71f74b7dfe320790592ab09e0c20
Tim1997 <timbkhn@gmail.com>
2023-05-12 18:04:09 +0700

    modify UI follow requirement

--- src/web/capstone-library-client/src/views/Order/Order.jsx
+++ src/web/capstone-library-client/src/views/Order/Order.jsx
@@ -113,7 +113,7 @@ export default function Order() {
       l.checked && orders.push({ id: l.id });
     });
     if (orders.length === 0) {
-      NotificationManager.error("Giỏ hàng của bạn đang trống", "Lỗi", 1000);
+      NotificationManager.error("Bạn chưa chọn bài đăng để thanh toán", "Lỗi", 1000);
     } else {
       const res = await dispatch(checkout({ orders }));
       if (res.success) {
